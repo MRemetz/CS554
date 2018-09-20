82ecfba1221cada6e50ba2fb98e0b7c3e66b6d0a
Max Remetz <max@remetz.com>
2018-09-20 13:17:54 -0400

    updated sass and gulpfile

--- assignment3/gulpfile.js
+++ assignment3/gulpfile.js
@@ -5,12 +5,10 @@ const autoPrefix = require("gulp-autoprefixer");
 const gulpSASS = require("gulp-sass");
 const rename = require("gulp-rename");
 
-//"./src/styles/bootstrap/scss/_variables.scss"
 
 const sassFiles = [
   "./src/styles/variables.scss",
   "./src/styles/custom.scss",
-  "./src/styles/bootstrap/scss/_variables.scss"
 ];
 
 const vendorJsFiles = [
