b2d0ca3e25ea30fffbaa0cdf6854807c8b9343c2
Max Remetz <max@remetz.com>
2018-10-12 00:50:31 -0400

    renamed projects and added markdown editor project

---
+++ markdown_editor/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const config = {
+    entry: {
+        app: './src/app.js'
+    },
+    devtool: "inline-source-map",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        hot: true,
+        port: 3000
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            inject: true,
+            template: "dist/index.html"
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
+
+module.exports = config;
