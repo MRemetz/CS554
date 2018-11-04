f923fd73eb5df329e0c4b506482e700df934b32e
Max Remetz <max@remetz.com>
2018-11-04 14:44:59 -0500

    removed extraneous line

--- user_cache/server.js
+++ user_cache/server.js
@@ -7,8 +7,6 @@ const app = express();
 const client = redis.createClient();
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
-bluebird.promisifyAll(redis.Multi.prototype);
-
 
 app.get("/api/people/history", async (req, res) => {
     await client
@@ -56,5 +54,5 @@ app.get("/api/people/:id", async (req, res) => {
 
 app.listen(3000, () => {
     console.log("Server up and running!");
-    console.log("Your routes will be running on http://localhost:300");
+    console.log("Your routes will be running on http://localhost:3000");
 });
