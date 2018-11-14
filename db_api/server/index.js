57a1abf282588627566061c324d6186d97d77b37
Max Remetz <max@remetz.com>
2018-11-13 22:32:40 -0500

    GET call working

--- db_api/server/index.js
+++ db_api/server/index.js
@@ -9,69 +9,82 @@ app.use(bodyParser.json());
 app.get("/api/people/:id", async (req, res) => {
     try {
         let response = await nrpSender.sendMessage({
-          redis: redisConnection,
-          eventName: "GET",
-          data: {
-            message: req.body.message
-          }
+            redis: redisConnection,
+            eventName: "GET",
+            data: {
+                id: req.params.id
+            }
         });
-    
+
         res.json(response);
-      } catch (e) {
-        res.json({ error: e.message });
-      }
+
+    } catch (e) {
+        res.json({
+            error: e.error
+        });
+    }
 });
 
 app.post("/api/people", async (req, res) => {
     try {
         let response = await nrpSender.sendMessage({
-          redis: redisConnection,
-          eventName: "POST",
-          data: {
-            message: req.body.message
-          }
+            redis: redisConnection,
+            eventName: "POST",
+            data: {
+                message: req.body.message
+            }
         });
-    
+
         res.json(response);
-      } catch (e) {
-        res.json({ error: e.message });
-      }
+
+    } catch (e) {
+        res.json({
+            error: e.error
+        });
+    }
 });
 
 app.delete("/api/people/:id", async (req, res) => {
     try {
         let response = await nrpSender.sendMessage({
-          redis: redisConnection,
-          eventName: "DELETE",
-          data: {
-            message: req.body.message
-          }
+            redis: redisConnection,
+            eventName: "DELETE",
+            data: {
+                message: req.params.id
+            }
         });
-    
-        res.json({message: "person with id: " + id + " successfully deleted"});
-      } catch (e) {
-        res.json({ error: e.message });
-      }
+
+        res.json({
+            message: "person with id: " + id + " successfully deleted"
+        });
+
+    } catch (e) {
+        res.json({
+            error: e.error
+        });
+    }
 });
 
 app.put("/api/people/:id", async (req, res) => {
     try {
         let response = await nrpSender.sendMessage({
-          redis: redisConnection,
-          eventName: "PUT",
-          data: {
-            message: req.body.message
-          }
+            redis: redisConnection,
+            eventName: "PUT",
+            data: {
+                id: req.params.id,
+                fields: req.body
+            }
         });
-    
+
         res.json(response);
-      } catch (e) {
-        res.json({ error: e.message });
-      }
+    } catch (e) {
+        res.json({
+            error: e.error
+        });
+    }
 });
 
 app.listen(3000, () => {
     console.log("We've now got a server!");
     console.log("Your routes will be running on http://localhost:3000");
-  });
-  
+});
--- db_api/worker/worker.js
+++ db_api/worker/worker.js
@@ -1,8 +1,49 @@
 const redisConnection = require("./redis-connection");
 
-const fetch = require('node-fetch');
-const Bluebird = require('bluebird');
+const axios = require('axios');
 
-fetch.Promise = Bluebird;
+async function loadData() {
+    const dataURL = 'https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json';
+    const res = await axios.get(dataURL);
+    dummyData = res.data;
+}
 
-const dataURL = 'https://gist.githubusercontent.com/philbarresi/5cf15393d245b38a2d86ce8207d5076c/raw/d529fb474c1af347702ca4d7b992256237fa2819/lab5.json';
+var dummyData = null;
+loadData();
+
+redisConnection.on("GET:request:*", async (message, channel) => {
+    let requestId = message.requestId;
+    let eventName = message.eventName;
+    var failedEvent = `${eventName}:failed:${requestId}`;
+    let successEvent = `${eventName}:success:${requestId}`;
+
+    if (dummyData === null) {
+        redisConnection.emit(failedEvent, {
+            requestId: requestId,
+            data: {
+                error: 'Still loading data. Please wait and try again'
+            },
+            eventName: eventName
+        });
+
+    } else {
+        let id = message.data.id;
+        for (var i in dummyData) {
+            if (dummyData[i] && dummyData[i]["id"] == id) {
+                redisConnection.emit(successEvent, {
+                    requestId: requestId,
+                    data: dummyData[i],
+                    eventName: eventName
+                });
+                break;
+            }
+        }
+        redisConnection.emit(failedEvent, {
+            requestId: requestId,
+            data: {
+                error: 'User not found'
+            },
+            eventName: eventName
+        });
+    }
+});
